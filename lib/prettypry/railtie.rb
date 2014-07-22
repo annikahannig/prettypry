730e20b641537803edf8c2a37aa1f60d790f584a
Matthias Hannig <hannig@trustner.com>
2014-07-22 13:00:03 +0200

    Updated color prompt

--- lib/prettypry/railtie.rb
+++ lib/prettypry/railtie.rb
@@ -2,6 +2,8 @@
 #
 # Prettypry Railtie
 # =================
+#
+# (c) 2014 Matthias Hannig, Trustner GmbH
 # 
 
 require 'pry'
@@ -10,16 +12,18 @@ require 'pry-rails'
 module Prettypry
   class Railtie < Rails::Railtie
     initializer 'prettypry.initialize' do |app|
-      
+
       # == Configure Pry Prompt to show app name
-      app_name = app.class.parent_name.underscore
+      app_name = app.class.parent_name.underscore.blueish
+      separator = '>'.redish
 
       main_prompt = proc do |obj, nest_level, _|
-        "#{app_name}> " 
+        line_num = "[#{_.input_array.size.to_s.blue}]"
+        "#{line_num} #{app_name.blueish} #{separator} " 
       end
 
       wait_prompt = proc do |obj, nest_level, _|
-        "> " 
+        "#{separator} " 
       end
 
       Pry.config.prompt = [
